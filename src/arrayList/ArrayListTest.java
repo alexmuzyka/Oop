763f1d29648c66de481ba398dbc256e7338422c3
User <J:\Soft\TheBat!\TheBat!>
2017-12-06 12:20:56 +0300

    added arrayList, hashtable

---
+++ src/arrayList/ArrayListTest.java
@@ -0,0 +1,32 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListTest {
+    public static void printList(List<String> words) {
+        for (int i = 0; i < words.size(); i++) {
+            System.out.println("Words[" + i + "]: " + words.get(i));
+        }
+    }
+ 
+    public static void main(String []args) {
+        List<String> words = new ArrayList<String>();
+        
+        words.add("school");
+        printList(words);
+ 
+        words.add(0, "at");
+        System.out.println("> Insert via index");
+        printList(words);
+ 
+        words.set(1, "work");
+        System.out.println("> Update via index");
+        printList(words);
+ 
+        words.remove(0);
+        System.out.println("> Remove via index");
+        printList(words);
+ 
+    }
+}
